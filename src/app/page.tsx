78a1ac15bd9c39ff211c0af369357bf3f261bf2a
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-06-13 00:27:34 -0300

    :sparkles: feat: Added header
    Added a background image to the header, created a text with the application name using the 'h1' tag, an 'input' for search, and a 'button' to submit the search.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen bg-black">
       <Header />
       <main>
-        <div className=""></div>
+        Main
       </main>
     </div>
   )
