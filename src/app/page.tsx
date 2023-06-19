478e63f73a166e3854d7e1c8370e61b4d328d715
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-06-19 06:11:32 -0300

    :recycle: refactor: Separated useState logic in HomeContainer
    Moved the useState logic from the home page to the HomeContainer component. Now, the home page only renders the HomeContainer, making it cleaner.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
 'use client'
-import { useState } from 'react';
-import Header from "@/components/Header";
-import Main from "@/components/Main";
-import { fetchLocationData, LocationData } from "../utils/api";
+import HomeContainer from '@/components/HomeContainer';
 
 export default function Home() {
-  const [locationData, setLocationData] = useState<LocationData | null>(null);
-
-  const handleLocationData = async (ipAddress?: string) => {
-    const data = await fetchLocationData(ipAddress);
-    setLocationData(data);
-  };
 
   return (
     <div className="bg-black text-slate-50">
-      <Header onSearch={handleLocationData} />
-      {locationData && (
-        <Main
-          location={{
-            lat: locationData.location.lat,
-            lng: locationData.location.lng
-          }}
-        />
-      )}
+      <HomeContainer />
     </div>
   );
 }
---
+++ src/components/HomeContainer.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { useState } from 'react';
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import { fetchLocationData, LocationData } from "../utils/api";
+
+export default function HomeContainer() {
+    const [locationData, setLocationData] = useState<LocationData | null>(null);
+
+    const handleLocationData = async (ipAddress?: string) => {
+        const data = await fetchLocationData(ipAddress);
+        setLocationData(data);
+    };
+
+    return (
+        <div className="bg-black text-slate-50">
+            <Header onSearch={handleLocationData} />
+            {locationData && (
+                <Main
+                    location={{
+                        lat: locationData.location.lat,
+                        lng: locationData.location.lng
+                    }}
+                />
+            )}
+        </div>
+    );
+}
